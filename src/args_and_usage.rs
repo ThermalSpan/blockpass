320dd46fda3dc8c5a5e3a6551eda4c36e31debf1
Russell Bentley <russell.w.bentley@icloud.com>
2017-09-30 13:19:55 -0700

    First commit and its working

---
+++ src/args_and_usage.rs
@@ -0,0 +1,69 @@
+use clap::{Arg, App};
+use std::path::PathBuf;
+
+// Programmer defined constants
+static PROGRAM_NAME: &'static str = "yuga";
+
+// Derived constants
+static VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+#[derive(Debug)]
+pub struct Args {
+    pub input: PathBuf,
+    pub output: PathBuf,
+    pub keep_orig: bool,
+    pub verbose: bool,
+    pub start_deliminator: String,
+    pub end_deliminator: String,
+}
+
+pub fn parse_args() -> Args {
+    let args = App::new(PROGRAM_NAME)
+        .version(VERSION)
+        .author("Russell W. Bentley <russell.w.bentley@icloud.com>")
+        .about("A tool for passing blocks from one file to another")
+        .arg(Arg::with_name("INPUT")
+            .help("The file to retreive the block from")
+            .long("input")
+            .value_name("input/file.ex")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("OUTPUT")
+            .help("The file to prepend the block to")
+            .long("output")
+            .value_name("output/fie.ex")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("KEEP_ORIG")
+            .help("Defualt behaivor is to delete original outout file")
+            .long("keep-orig"))
+        .arg(Arg::with_name("VERBOSE")
+            .help("Print out extra info")
+            .long("verbose"))
+        .arg(Arg::with_name("START_DELIMINATOR")
+            .help("The deliminator that starts the block")
+            .long("start-delim")
+            .default_value("---"))
+        .arg(Arg::with_name("END_DELIMINATOR")
+            .help("The deliminator that ends the block")
+            .long("end-delim")
+            .default_value("---"))
+        .get_matches();
+
+    let input_path_raw = args.value_of("INPUT").unwrap();
+    let input_path = PathBuf::from(input_path_raw);
+
+    let output_path_raw = args.value_of("OUTPUT").unwrap();
+    let output_path = PathBuf::from(output_path_raw);
+
+    Args {
+        input: input_path,
+        output: output_path,
+        keep_orig: args.is_present("KEEP_ORIG"),
+        verbose: args.is_present("VERBOSE"),
+        start_deliminator: String::from(args.value_of("START_DELIMINATOR").unwrap()),
+        end_deliminator: String::from(args.value_of("END_DELIMINATOR").unwrap()),
+    }
+}
+
+
---
+++ src/main.rs
@@ -0,0 +1,122 @@
+extern crate clap;
+#[macro_use] extern crate error_chain;
+
+mod args_and_usage;
+
+use args_and_usage::parse_args;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
+
+quick_main!(|| -> Result<()> {
+    let args = parse_args();
+
+    let input_file = File::open(&args.input)
+        .chain_err(|| format!("Can't open input file: {}", args.input.display()))?;
+    let mut input_reader = BufReader::new(input_file);
+
+    // First lets set up the stuff we need to read in the block
+    // A buffer to store the block we read in
+    let mut block_buffer = String::new();
+    // A buffer to store each line as we read it
+    let mut line_buffer = String::new();
+
+    // For our use case, the first line MUST be the start deliminator
+    input_reader.read_line(&mut line_buffer)?;   
+
+    if chomp(&line_buffer) != args.start_deliminator {
+        println!("{} vs {} : wat", chomp(&line_buffer), args.start_deliminator);
+        bail!(ErrorKind::NoStartDeliminator);
+    }
+    
+    // Now we iterate through the remaining lines
+    // Until we hit the end deliminator
+    loop {
+        line_buffer.clear();    
+        input_reader.read_line(&mut line_buffer)?;
+        
+        //push that line into the block_buffer
+        block_buffer.push_str(&line_buffer);
+
+        if args.verbose {
+            println!("Copied Line to block: {}", line_buffer);
+        }
+
+        // If we see the end_delimnator we're done
+        if chomp(&line_buffer) == args.end_deliminator {
+            break;
+        }
+    }
+
+    // For our use case, the block MUST be non empty
+    if block_buffer.is_empty() {
+        bail!(ErrorKind::EmptyBlock);
+    }
+
+    // The first step is to move the original file aside
+    let mut original_output_path = args.output.clone();
+    if ! original_output_path.set_extension("orig") {
+        bail!(ErrorKind::UnableToAddOrigExtension);
+    }
+    fs::rename(&args.output, &original_output_path)
+        .chain_err(|| format!("Unable to rename\n{}\nto\n{}", args.output.display(), original_output_path.display()))?;
+
+    if args.verbose {
+        println!("Renamed output file\n\t{}\n->\n\t{}", args.output.display(), original_output_path.display());
+    }
+    
+    // No lets write out
+    let mut output_file = File::create(&args.output)
+        .chain_err(|| format!("Can't create ouput file: {}", args.input.display()))?;
+
+    let mut orig_file = File::open(&original_output_path)
+        .chain_err(|| format!("Can't open orig file: {}", original_output_path.display()))?;
+
+    // First write out the block
+    output_file.write_all(block_buffer.as_bytes())?;
+
+    //No we need to copy contents of orig_file to output_file
+    let mut output_buffer = String::new();
+    orig_file.read_to_string(&mut output_buffer)
+        .chain_err(|| format!("Can't read contents from {}", original_output_path.display()))?;
+
+    output_file.write_all(output_buffer.as_bytes())
+        .chain_err(|| format!("Can't write to {}", args.output.display()))?;
+
+    if ! args.keep_orig {
+        fs::remove_file(&original_output_path)
+            .chain_err(|| format!("Unable to delete original {}", original_output_path.display()))?;
+    }
+
+    Ok(())
+});
+
+// Use this to remove a trailing newline, if there is one
+fn chomp(input: &str) -> String{
+    let mut result = String::from(input);
+
+    match result.pop() {
+        None | Some('\n') => (),
+        Some(c) => result.push(c)
+    }
+
+    result
+}
+
+error_chain! {
+    errors {
+        NoStartDeliminator {
+            description("First line of input was not the start deliminator")
+        }
+        EmptyBlock {
+            description("The block in the input file was empty")
+        }
+        UnableToAddOrigExtension {
+            description("We were unable to add the .orig extension the ouput path")
+        }
+    }
+    foreign_links {
+        IO(std::io::Error);
+    }
+}
